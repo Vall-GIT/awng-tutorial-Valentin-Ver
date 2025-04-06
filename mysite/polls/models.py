1c4ae67265cd928b6f85cc62f7d342633389025d
Valoxe <Valentin.VERRIEZ@student.isae-supaero.fr>
2025-04-06 15:20:20 +0200

    [Tuto] Etape 7 - Complétée : Templates et pages admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.utils import timezone
 from django.db import models
-
+from django.contrib import admin
 
 # Create your models here.
 
@@ -16,7 +16,14 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
